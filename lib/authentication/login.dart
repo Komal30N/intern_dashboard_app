f9a84488a09b4712764c253287913bb0c39b20ef
Komal30N <komal30n@gmail.com>
2025-08-01 23:01:11 +0530

    animation

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:intern_dash/authentication/sign up.dart';
-import 'package:intern_dash/pages/Dash_Page.dart';
 import 'package:intern_dash/pages/Homepage.dart';
 
 class login extends StatefulWidget {
@@ -14,45 +13,48 @@ class _loginState extends State<login> {
   TextEditingController email = TextEditingController();
   TextEditingController password = TextEditingController();
   TextEditingController username = TextEditingController();
+
+  bool _isPressed = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body:  Container(
+      body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-
         color: Colors.grey[300],
         child: Padding(
-          padding: const EdgeInsets.only(top:80.0),
+          padding: const EdgeInsets.only(top: 80.0),
           child: SingleChildScrollView(
             child: Column(
-              mainAxisAlignment:MainAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-               Container(
-                 height: 75,
-                 width: 75,
-                 child: Icon(Icons.lock_open , size: 100, color: Colors.grey[500],),),
-                SizedBox(height: 45,),
+                Container(
+                  height: 75,
+                  width: 75,
+                  child: Icon(
+                    Icons.lock_open,
+                    size: 100,
+                    color: Colors.grey[500],
+                  ),
+                ),
+                const SizedBox(height: 45),
                 Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: TextField(
                     controller: username,
                     decoration: InputDecoration(
-                        hintText: 'Username',
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                            )
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                              color: Colors.grey,
-                            )
-                        )
+                      hintText: 'Username',
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.black),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
                     ),
                   ),
                 ),
@@ -61,24 +63,18 @@ class _loginState extends State<login> {
                   child: TextField(
                     controller: email,
                     decoration: InputDecoration(
-                        hintText: 'Email',
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                            )
-                        ),
+                      hintText: 'Email',
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.black),
+                      ),
                       enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                          color: Colors.grey,
-                        )
-                      )
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
                     ),
                   ),
                 ),
-            
-
                 Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: TextField(
@@ -87,79 +83,126 @@ class _loginState extends State<login> {
                     decoration: InputDecoration(
                       hintText: 'Password',
                       focusColor: Colors.black,
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                            )
-                        ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.black),
+                      ),
                       enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                          color: Colors.grey,
-                        )
-                      )
-            
-                    )
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                  const EdgeInsets.only(right: 15.0, top: 5, bottom: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Forgot Password ?",
+                        style: TextStyle(
+                            color: Colors.grey[600], fontSize: 15),
+                      ),
+                    ],
                   ),
                 ),
 
-                 Padding(
-                   padding: const EdgeInsets.only(right: 15.0 , top: 5 ,bottom: 15),
-                   child: Row(
-                     mainAxisAlignment: MainAxisAlignment.end,
-                     children: [
 
-                       Text("Forgot Password ?" ,style: TextStyle(color: Colors.grey[600], fontSize: 15),),
-                     ],
-                   ),
-                 ),
-            
-               // ElevatedButton(onPressed: (){}, child: Text('LOGIN' ,style: TextStyle( backgroundColor :Colors.white , fontSize: 20, color:Colors.black))),
-               GestureDetector(
-                 onTap: () { Navigator.pushReplacement(context,
-                     MaterialPageRoute(
-
-                         builder: (context)=>Homepage( Name: username.text,),
-
-                       ));},
-
-                    child: Padding(
-                      padding: const EdgeInsets.only(top : 20.0 , left: 10, right: 10 , bottom: 10),
+                GestureDetector(
+                  onTap: () async {
+                    setState(() {
+                      _isPressed = true;
+                    });
+                    await Future.delayed(const Duration(milliseconds: 100));
+                    setState(() {
+                      _isPressed = false;
+                    });
+                    await Future.delayed(const Duration(milliseconds: 100));
+                    Navigator.pushReplacement(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            Homepage(Name: username.text),
+                        transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                          return FadeTransition(
+                            opacity: animation,
+                            child: child,
+                          );
+                        },
+                        transitionDuration: const Duration(milliseconds: 500),
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 20.0, left: 10, right: 10, bottom: 10),
+                    child: AnimatedScale(
+                      duration: const Duration(milliseconds: 100),
+                      scale: _isPressed ? 0.95 : 1.0,
                       child: Container(
                         width: double.infinity,
-                                         decoration: BoxDecoration( color: Colors.black,borderRadius: BorderRadius.circular(10),),
-                                         child: Center(child: Padding(
-                                           padding: const EdgeInsets.all(8.0),
-                                           child: Text('LOGIN' , style: TextStyle(fontSize: 20, color:Colors.white, fontFamily: 'Roboto', fontWeight: FontWeight.bold)),
-                                         )),
-                                       ),
-                    )
-               ),
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Text(
+                              'LOGIN',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontFamily: 'Roboto',
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
 
+                /// Sign Up Row
                 Padding(
                   padding: const EdgeInsets.only(top: 40.0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("New User ? ", style: TextStyle(color: Colors.grey[600], fontSize: 20),),
+                      Text(
+                        "New User ? ",
+                        style: TextStyle(
+                            color: Colors.grey[600], fontSize: 20),
+                      ),
                       GestureDetector(
-                        onTap: () {Navigator.push(context , MaterialPageRoute(builder: (context) => const signup()));  },
-                          child: Text("Sign Up", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),)
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const signup()),
+                          );
+                        },
+                        child: const Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       )
                     ],
                   ),
                 )
-
-
-
               ],
-
             ),
           ),
         ),
       ),
-      
     );
   }
 }
